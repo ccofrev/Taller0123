78d952b251e966210579c88fc15f8a4bf0d0d44a
ccofrev <carlos.cofre@usach.cl>
2023-06-05 02:31:53 -0400

    Quita tareas

--- Clases/clase0527/assets/js/scripts.js
+++ Clases/clase0527/assets/js/scripts.js
@@ -67,8 +67,17 @@ console.log("tipo numero", typeof Number(doce))
 console.log("tipo numero", typeof parseInt(doce))
 
 // tarea
-// notas = [6,8,9,2,5,10]
-// function suma = () => {
-//     for(let n in notas)
-//         suma = suma + n
-// }
+let sumaArreglo = (notas) => {
+    let s = 0
+    for(let i=0; i<notas.length; i++)
+        s = s + notas[i]
+    return s
+}
+
+let promedio = (sumador, notas) => {
+    return sumador(notas)/notas.length
+}
+
+nots = [6,8,9,2,5,10]
+console.log(suma(nots))
+console.log(promedio(suma,nots))
